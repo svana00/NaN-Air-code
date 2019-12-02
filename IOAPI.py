917fcbf6be6252313aa3cd1223a81d210e0b5a8e
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2019-12-02 22:05:01 +0000

    Load all staff through IOAPI

--- IOAPI.py
+++ IOAPI.py
@@ -1,6 +1,7 @@
-from 
+from Staff_Members_IO import StaffMemberIO
 
 class IOAPI():
     
-    def load_all_staff_from_file():
-        return Staff_Members_IO.load_all_staff_from_file()
+    def load_all_staff_from_file(self):
+        StaffMemberIO_temp = StaffMemberIO()
+        return StaffMemberIO_temp.load_all_staff_from_file()
