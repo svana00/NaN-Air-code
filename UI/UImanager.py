3d83dff1224126cec6fc03fc7e464f84b5375ee1
EvaNaabye <evan19@ru.is>
2019-12-02 19:33:47 +0000

    gosh

--- UI/UImanager.py
+++ UI/UImanager.py
@@ -7,9 +7,10 @@ class UIManager:
         while True:
             print("*"*26 + "\n\t NaN AIR\n"+"*"*26)
             print("1. STAFF\n2. AIRPLANES\n3. VOYAGES\n4. DESTINATIONS")
-
-
             var = input("\nInput a command: ")
-            print("your command was: " + var)
+            if var == "1":
+                print("amazing choice m8")
+            if var != "1" or "2" or "3" or "4":
+                print("invalid choice")
 
 
