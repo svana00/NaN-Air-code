bbd7ed7f26fe988115f68471eb4693683812f931
svana00 <54863023+svana00@users.noreply.github.com>
2019-12-03 18:37:26 +0000

    t

--- StaffMemberLL.py
+++ StaffMemberLL.py
@@ -6,30 +6,6 @@ class StaffMemberLL():
     def get_staff_member_info(self):
         pass
 
-    def get_all_pilots(self):
-        staff_list = self.IOAPI_temp.load_all_staff_from_file()
-        pilots_info_list = []
-
-        for staff_member in staff_list:
-            if staff_member.role == "Pilot":
-                ssn = staff_member.get_ssn()
-                name = staff_member.get_name()
-                pilots_info_list.append((ssn, name))
-
-        return pilots_info_list
-
-    def get_all_flight_attendants(self):
-        staff_list = self.IOAPI_temp.load_all_staff_from_file()
-        pilots_info_list = []
-
-        for staff_member in staff_list:
-            if staff_member.role == "Flight attendant":
-                ssn = staff_member.get_ssn()
-                name = staff_member.get_name()
-                pilots_info_list.append((ssn, name))
-
-        return pilots_info_list
-
     def get_all_staff(self):
         ''' Returns a list of tuples with names and ssn of all staff members '''
         staff_list = self.IOAPI_temp.load_all_staff_from_file()
@@ -42,14 +18,48 @@ class StaffMemberLL():
         
         return staff_info_list
 
-    def get_pilots_by_license(self):
-        pass
+    def get_all_flight_attendants(self):
+        staff_list = self.IOAPI_temp.load_all_staff_from_file()
+        flight_attendants_info_list = []
+
+        for flight_attendant in staff_list:
+            if flight_attendant.role == "Flight attendant":
+                ssn = flight_attendant.get_ssn()
+                name = flight_attendant.get_name()
+                flight_attendants_info_list.append((ssn, name))
+
+        return flight_attendants_info_list
+
+    def get_pilots(self):
+        ''' Returns a list of all instances of pilots '''
+        staff_list = self.IOAPI_temp.load_all_staff_from_file()
+        pilots_list = []
+
+        for staff_member in staff_list:
+            if staff_member.role == "Pilot":
+                pilots_list.append(staff_member)
+            
+        return pilots_list
+
+    def get_all_pilots(self):
+        ''' Returns a list of tuples for each pilot, the tuple contains their ssn and name '''
+        pilots_list = self.get_pilots()
+        pilots_info_list = []
+
+        for pilot in pilots_list:
+            ssn = pilot.get_ssn()
+            name = pilot.get_name()
+            pilots_info_list.append((ssn, name))
+
+        return pilots_info_list
 
     def get_pilots_by_one_license(self):
-        pass
+        pilots_list = self.get_pilots()
+        pilots_info_list = []
 
     def get_pilots_by_all_licenses(self):
-        pass
+        pilots_list = self.get_pilots()
+        pilots_info_list = []
 
     def create_staff_members(self):
         pass
