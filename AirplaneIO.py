2e678e69cfa94e237937882895a1a57f6dd3a635
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2019-12-03 19:07:43 +0000

    Ariplanetype í DL

--- AirplaneIO.py
+++ AirplaneIO.py
@@ -1,5 +1,6 @@
 import csv
 from models import Airplane
+from models import AirplaneType
 
 class AirplaneIO:
     def load_all_airplanes(self):
@@ -24,3 +25,20 @@ class AirplaneIO:
             airplanes_list.append(airplane)
 
         return airplanes_list
+    
+    def load_airplane_types(self):
+        types_list = []
+
+        planeType_file = open("AirplaneType.csv", "r")
+        type_reader = csv.DictReader(planeType_file)
+
+        for row in type_reader:
+            type_id = row["planeTypeId"]
+            type_manufacturer = row["manufacturer"]
+            type_model = ["model"]
+            type_capacity = ["capacity"]
+
+            airplaneType = AirplaneType(type_id, type_manufacturer, type_model, type_capacity)
+            types_list.append(airplaneType)
+
+        return types_list
