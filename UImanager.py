34897ffe58425d501829abf31ac3ecc167d893f5
EvaNaabye <evan19@ru.is>
2019-12-03 16:18:27 +0000

    sdfgh

--- UImanager.py
+++ UImanager.py
@@ -11,7 +11,7 @@ class UIManager():
         
     def mainLoop(self):
         while True:
-            print("*"*26 + "\n\t NaN AIR\n"+"*"*26)
+            print("\n\n" + "*"*26 + "\n\t NaN AIR\n"+"*"*26)
             print("1. STAFF\n2. AIRPLANES\n3. VOYAGES\n4. DESTINATIONS")
             var = input("\nInput a command: ")
             if var == "1":
