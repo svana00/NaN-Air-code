3cebb98a0e98ce365732c271c345a2b82730895c
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2019-12-03 13:13:16 +0000

    whoopwhoop

--- LLAPI.py
+++ LLAPI.py
@@ -4,9 +4,11 @@ from AirplaneLL import AirplaneLL
 from DestinationLL import DestinationLL
 
 class LLAPI():
-
+    
     def get_all_staff(self):
-        return StaffMemberLL.get_all_staff(self)
+        Staff_Member_LL = StaffMemberLL()
+        return Staff_Member_LL.get_all_staff()
 
     def get_all_dest(self):
-        return DestinationLL.getDestinations(self)
+        Destination_LL = DestinationLL()
+        return Destination_LL.getDestinations()
