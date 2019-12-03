3e7eee53381054436cc524973fa611fa20d51609
svana00 <54863023+svana00@users.noreply.github.com>
2019-12-03 12:04:05 +0000

    grrr

---
+++ AirplanesLL.py
@@ -0,0 +1,11 @@
+from models import Airplane
+
+class AirplanesLL():
+    def getallAirplaneTypes(self):
+        pass
+
+    def getAirplaneState(self):
+        pass
+
+    def makeAirplane(self):
+        pass
---
+++ VoyagesLL.py
@@ -0,0 +1,26 @@
+from models import Voyage
+
+class VoyagesLL():
+    def assignStaff(self):
+        pass
+
+    def makeVoyage(self):
+        pass
+
+    def getVoyage(self):
+        pass
+
+    def getVoyagesbyDate(self):
+        pass
+
+    def getVoyagesbyWeek(self):
+        pass
+
+    def getnonAssignedVoyages(self):
+        pass
+
+    def checkifFullyAssigned(self):
+        pass
+
+    def checkVoyagestate(self):
+        pass
