de6a49f506bb8e91706c3103770061992f5a5e94
EvaNaabye <evan19@ru.is>
2019-12-04 12:18:23 +0000

    dsfdf

--- DestinationUI.py
+++ DestinationUI.py
@@ -76,10 +76,10 @@ class DestinationUI():
 
             elif choice == "confirm":
                 break
+
             self.header("-", " ADD DESTINATION ")
             print("\n1. COUNTRY: {}\n2. CITY: {}\n3. AIRPORT: {}\n4. FLIGHT TIME: {}\n5. DISTANCE: {}\n6. NAME OF CONTACT: {}\n7. EMERGENCY PHONE: {}".format(country_str, city_str, airport_str, flight_time_str, distance_str, name_of_contact_str, emergency_number_str))
             choice = input("\nInput what you want to add: ")
-        
         
         return self.llAPI.create_new_destination(destination_info_list)
 
