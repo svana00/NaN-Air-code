05117c341d86224b9ac20e7c2e066f113531b8b5
AntonMayboeck <antonmayboeck@gmail.com>
2019-12-05 17:31:21 +0000

    Added flight number id to dest.csv

--- Validation/validation.py
+++ Validation/validation.py
@@ -1,1 +1,20 @@
-#class Validate:
+class Validate:
+    def __init__(self):
+        pass
+
+    def check_if_letters(self):
+        a_str = "hello is this the new number"
+        new_str = a_str.replace(" ", "")
+        if new_str.isalpha() == True:
+            return a_str
+        
+    def check_if_number(self, a_str):
+        new_str = a_str.replace(" ", "")
+        if new_str.isdigit() == True:
+            return a_str
+
+    def check_ssn(self)
+
+    def check_phonenumber(self)
+
+    def check_id(self)
