436905efbd94be30a77031357abc8390b3134f20
AntonMayboeck <antonmayboeck@gmail.com>
2019-12-06 14:24:07 +0000

    Made every function in validate class and tested datetime

--- Validation/test.py
+++ Validation/test.py
@@ -1,3 +1,22 @@
+import datetime
+
 a_str = "1 2 3 4 5 6"
 b_str = a_str.replace(" ", "")
 print(b_str.isdigit())
+
+nde_str = "sigurdur@nanair.is"
+siggf  = nde_str.split("@")
+print(siggf)
+nd = int(1)
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month "))
+day = int(input("Enter a day: "))
+hour = int(input("Enter a hour: "))
+minute = int(input("Enter a minute: "))
+second = int(input("Enter a second: "))
+for index in range(7):
+    c = datetime.datetime(year, month, day+index, hour, minute, second).isoformat()
+    
+    print(c)
+
--- Validation/validation.py
+++ Validation/validation.py
@@ -3,18 +3,61 @@ class Validate:
         pass
 
     def check_if_letters(self):
-        a_str = "hello is this the new number"
         new_str = a_str.replace(" ", "")
         if new_str.isalpha() == True:
             return a_str
+        else:
+            return -1
         
     def check_if_number(self, a_str):
         new_str = a_str.replace(" ", "")
         if new_str.isdigit() == True:
             return a_str
+        else:
+            return -1
 
-    def check_ssn(self)
+    def check_ssn(self, a_str):
+        if len(a_str) == 10 and a_str.isdigit():
+            return a_str
+        else:
+            return -1
 
-    def check_phonenumber(self)
+    def check_phonenumber(self):
+        if len(a_str) == 7 and a_str.isdigit():
+            return a_str
+        else:
+            return -1
 
     def check_id(self)
+        pass
+
+    def check_email(self):
+        #take the input and split by @ and by .
+        #check if the first is all letters
+        #check if the second is nanair.is
+        #
+
+    def check_address(self):
+        pass
+
+    def plane_type(self):
+        pass
+
+    def plane_id(self):
+        pass
+
+    def check_time(self):
+        pass
+
+    def check_if_working(self):
+        pass
+
+    def check_destination(self):
+        pass
+
+    def check_arrival(self):
+        pass
+
+    def check_flight_num(self):
+        pass
+
