99424e0f76ef4c8ea545a93474566358953c838c
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2019-12-09 16:39:21 +0000

    ef þið fokkið þessu upp þá drep ég ykkur og svo mig vol 2

--- MODELS/voyage.py
+++ MODELS/voyage.py
@@ -54,6 +54,7 @@ class Voyage():
 
     def __str__(self):
         my_str = ""
+        my_str += "Voyage ID: {}\n".format(self.__voyage_id)
         my_str += "Flight number out: {}\n".format(self.__flight_number_out)
         my_str += "Flight number back: {}\n".format(self.__flight_number_back)
         my_str += "Departure out: {}\n".format(self.__departure_out)
