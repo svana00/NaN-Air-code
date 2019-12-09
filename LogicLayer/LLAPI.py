9d0cda4a25223f1d6387749313a5e8922a0aa404
svana00 <54863023+svana00@users.noreply.github.com>
2019-12-09 14:28:31 +0000

    llapi skipulag

--- LogicLayer/LLAPI.py
+++ LogicLayer/LLAPI.py
@@ -13,6 +13,7 @@ class LLAPI():
         self.voyageLL = VoyageLL(self.ioAPI)
         self.airplaneLL = AirplaneLL(self.ioAPI)
 
+    #---- Staff member functions ----
     def get_staff_member_info(self, ssn):
         return self.staffLL.get_staff_member_info(ssn)
 
@@ -27,7 +28,26 @@ class LLAPI():
 
     def get_all_staff(self):
         return self.staffLL.get_all_staff()
+    
+    def get_all_pilots(self):       
+        return self.staffLL.get_all_pilots()
 
+    def get_all_flight_attendants(self):
+        return self.staffLL.get_all_flight_attendants()
+
+    def create_staff_member(self, staff_member_info_list):
+        return self.staffLL.create_staff_member(staff_member_info_list)
+
+    def get_all_working(self, desired_date_str):
+        return self.staffLL.get_all_working(desired_date_str)
+
+    def get_all_not_working(self, desired_date_str):
+        return self.staffLL.get_all_not_working(desired_date_str)
+
+    def get_staff_member_schedule(self, ssn, start_of_target_week):
+        return self.staffLL.get_staff_member_schedule(ssn, start_of_target_week)
+
+    # ---- Destination functions ----
     def get_destinations(self):
         return self.destLL.get_destinations()
 
@@ -41,27 +61,11 @@ class LLAPI():
         self.dest_list = dest_list
         return self.destLL.create_new_destination(self.dest_list)
 
-    def get_all_pilots(self):
-        return self.staffLL.get_all_pilots()
-
-    def get_all_flight_attendants(self):
-        return self.staffLL.get_all_flight_attendants()
-
-    def create_staff_member(self, staff_member_info_list):
-        return self.staffLL.create_staff_member(staff_member_info_list)
-
+    # ---- Voyages functions ----
     def get_all_voyages_for_week(self):
         return self.ioAPI.get_all_voyages_for_week(start_of_desired_week_str)
 
+    # ---- Airplane functions ----
     def create_new_airplane(self, airplane_str):
         self.airplane_str = airplane_str
         return self.airplaneLL.makeAirplane(airplane_str)
-
-    def get_all_working(self, desired_date_str):
-        return self.staffLL.get_all_working(desired_date_str)
-
-    def get_all_not_working(self, desired_date_str):
-        return self.staffLL.get_all_not_working(desired_date_str)
-
-    def get_staff_member_schedule(self, ssn, start_of_target_week):
-        return self.staffLL.get_staff_member_schedule(ssn, start_of_target_week)
