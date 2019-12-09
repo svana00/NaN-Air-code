32bb1d7843cfff869325eda29b7685de45524696
Margret <margreta19@ru.is>
2019-12-09 14:15:51 +0000

    uppfæra validation.py

--- Validation/validation.py
+++ Validation/validation.py
@@ -2,44 +2,48 @@ import datetime
 
 class Validate:
     def __init__(self):
-        # bæta við 
         pass
 
     def validate_letter(self, letter_str):
         new_str = letter_str.replace(" ", "")
         if new_str.isalpha() == True:
-            return letter_str
+            return True
         else:
-            return -1
+            return False
         
     def validate_number(self, num_str):
         new_str = num_str.replace(" ", "")
         if new_str.isdigit() == True:
-            return num_str
+            return True
         else:
-            return -1
+            return False
 
     def validate_ssn(self, ssn_num):
-        if len(ssn_num) == 10 and ssn_str.isdigit():
-            return ssn_num
+        if len(ssn_num) == 10 and ssn_num.isdigit():
+            return True
         else:
-            return -1
+            return False
 
     def validate_phone_num(self, phone_num_int):
-        if len(phone_num_int) == 7 and a_str.isdigit():
-            return a_str
+        if len(phone_num_int) == 7 and phone_num_int.isdigit():
+            return True
         else:
-            return -1
+            return False
 
     def validate_email(self, email_str):
-        #take the input and split by @ and by .
-        #check if the first is all letters
-        #check if the second is nanair.is
-        #
-        pass
+        if "@" in email_str:
+            temp_email_list = email_str.split("@")
+            if len(temp_email_list) == 2: 
+                if temp_email_list[1] == "nanair.is":
+                    return True
+        return False
 
     def validate_address(self, address_str):
-        pass
+        temp_address_list = address_str.split(" ")
+        if len(temp_address_list) == 2:
+            if temp_address[0].isalpha() and temp_address[1].isdigit():
+                return True
+        return False
 
     def validate_plane_type(self):
         pass
@@ -52,7 +56,7 @@ class Validate:
     def validate_time(self, time):
 
         try:
-            datetime.time(time)
+            datetime.time.fromisoformat(time)
             return True
         except ValueError:
             return False
@@ -82,4 +86,18 @@ class Validate:
 
     def validate_flight_num(self, flight_num_str):
         pass
+    def validate_staff_working(self, staff_member_id, be):
+        #voyage_list = self.llAPI.get_all_voyages()
+        #for voyage in voyage_list:
+        #    temp_staff_member_list = voyage.get_staff_members()
+        #    if staff_member_id in temp_staff_member_list:
+        #         return False
+        pass
 
+"""
+hellu = Validate()
+if hellu.validate_date("2000-03-18"):
+    print("yaaaaaaaaas")
+else:
+    print("noooopeeeee")
+"""
