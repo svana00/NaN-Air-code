f269b31d554030de38e9cec52e9ba46f872dad3a
EvaNaabye <evan19@ru.is>
2019-12-09 12:36:48 +0000

    model string

--- MODELS/voyage.py
+++ MODELS/voyage.py
@@ -47,3 +47,20 @@ class Voyage():
 
     def get_cabin_crew(self):
         return [self.__captain, self.__copilot, self.__fsm, self.__fa1, self.__fa2]
+
+    def __str__(self):
+        my_str += "Flight number out: {}".format(self.__flight_number_out)
+        my_str += "Flight number back: {}".format(self.__flight_number_back)
+        my_str += "Departure out: {}".format(self.__departure_out)
+        my_str += "Arrival out: {}".format(self.__arrival_out)
+        my_str += "Departue home: {}".format(self.__departure_home)
+        my_str += "Arrival home: {}".format(self.__arrival_home)
+        my_str += "Destination ID: {}".format(self.__destination_id)
+        my_str += "Plane ID: {}".format(self.__plane_id)
+        my_str += "Captain: {}".format(self.__captain)
+        my_str += "Co-pilot: {}".format(self.__copilot)
+        my_str += "Flight service manager: {}".format(self.__fsm)
+        my_str += "Flight attendant: {}".format(self.__fa1)
+        my_str += "Flight attendant: {}".format(self.__fa2)
+
+        return my_str
