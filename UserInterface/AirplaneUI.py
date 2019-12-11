50a8f0de2b06dc1a0ed4ffbb51c20bfbe8702198
Margret <margreta19@ru.is>
2019-12-11 21:37:12 +0000

    laga airplane state dótið

--- UserInterface/AirplaneUI.py
+++ UserInterface/AirplaneUI.py
@@ -75,11 +75,11 @@ class AirplaneUI():
             airplane_name_str = airplane.get_name()
             airplane_id_str = airplane.get_plane_id()
             airplane_type_str =  airplane.get_type_id()    #2019-11-12T06:20:00
-            chosen_time = datetime.datetime(2019,11,12,6,20,0)
+            chosen_time = datetime.datetime(2019,11,12,14,0,0)
             airplane_state_str = self.llAPI.get_airplane_state(airplane.get_plane_id(), chosen_time)
             print("{}. Name: {:<30} ID: {:<10} Type: {:<20} State: {:<15}".format(counter, airplane_name_str, airplane_id_str, airplane_type_str, airplane_state_str))
 
-
+# flug númer 10 NA10 NA11 TFBKL ----> BUKOLLA
     def show_airplane_state(self):
         #chosen_time = input("enter time")
         pass
