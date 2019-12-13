f2d581cccbc251b818e0fc3d20c1194a02a4b313
EvaNaabye <evan19@ru.is>
2019-12-13 23:46:17 +0000

    jhjhgh

--- LogicLayer/AirplaneLL.py
+++ LogicLayer/AirplaneLL.py
@@ -10,7 +10,7 @@ class AirplaneLL():
         return airplane_info_list
 
     def get_all_airplane_types(self):
-        ''' Returns a list of all airplane types '''
+        ''' Returns a list of all instances of airplane types '''
 
         airplane_types_list = self.ioAPI.load_airplane_types()
         airplane_types_info_list = []
@@ -22,7 +22,6 @@ class AirplaneLL():
         return airplane_types_info_list
 
     def get_airplane(self, plane_id):
-        ''' Filters out a single airplane using its ID '''
         airplanes_list = self.ioAPI.load_all_airplanes()
         
         for airplane in airplanes_list:
@@ -30,53 +29,41 @@ class AirplaneLL():
                 return airplane
 
     def make_airplane(self, new_airplane):
-        ''' Sends a new instance of an airplane down to the database for storing '''
-
         return self.ioAPI.store_new_airplane(new_airplane)
 
-    def get_airplane_state(self, airplane_id, chosen_time_and_date):
-        ''' Gets an airplane and chosen time and returns the state of the chosen airplane '''
+    def get_airplane_state(self, airplane_instance, chosen_time_and_date):
+        """ gets an airplane and chosen time and returns the state of the chosen airplane """
 
-        chosen_airplane = airplane_id
+        chosen_airplane = airplane_instance
         voyages_list = self.ioAPI.load_all_voyages() # List of all voyages
         airplane_state = "IDLE" # initializes the airplane state at IDLE
-        NOW = datetime.datetime.fromisoformat(chosen_time_and_date) # Makes the input date and time from user a datetime object
+        NOW = datetime.datetime.fromisoformat(chosen_time_and_date)
 
         for voyage in voyages_list:
             voyage_plane = voyage.get_plane_id()
 
-            # Gets all timestamps from a single voyage including when it will be available again...
-            # ... which is an hour after it arrives back to Keflavik.
             departure_out = datetime.datetime.fromisoformat(voyage.get_departure_out())
             arrival_out = datetime.datetime.fromisoformat(voyage.get_arrival_out())
             departure_home = datetime.datetime.fromisoformat(voyage.get_departure_home())
             arrival_home = datetime.datetime.fromisoformat(voyage.get_arrival_home())
             available = arrival_home + datetime.timedelta(hours = 1)
             
-        # If the chosen airplane matches the airplane of the voyage and the chosen datetime object is within the timefram of the voyage..
-        # .. The loop breaks and the state is updated.
             if voyage_plane == chosen_airplane:
                 if departure_out <= NOW and arrival_home > NOW:
                     break 
-        # If no matches are found, the plane is idle
         else:
             return airplane_state
             
-        # If the airplane is on its way from KEF:
         if departure_out <= NOW and NOW <= arrival_out:
             airplane_state = "Flight {} is on its way to {} and will be available again on: {}".format(voyage.get_flight_number_out(), voyage.get_dest_id(), available)
-        # If the airplane is in intermission:
         elif departure_home <= NOW and NOW <= arrival_home:
             airplane_state = "Flight {} is on its way to KEF and will be available again on: {}".format(voyage.get_flight_number_back(), available)
-        # If the airplane is on its way back to KEF
         elif arrival_out <= NOW and NOW <= departure_home:
             airplane_state = "IN INTERMISSION" 
 
         return airplane_state
 
     def get_free_airplanes(self, departure_out_str, arrival_home_str):
-        ''' Returns a list of all the plane ID's of the planes that are available '''
-
         voyages_list = self.ioAPI.load_all_voyages() # List of voyages
         airplanes_list = self.get_all_airplanes()
         plane_id_list = []
@@ -85,12 +72,10 @@ class AirplaneLL():
             plane_id = airplane.get_plane_id()
             plane_id_list.append(plane_id)
         
-        # Converts to datetime objects
         departure_out = datetime.datetime.fromisoformat(departure_out_str)
         arrival_home = datetime.datetime.fromisoformat(arrival_home_str)
 
-        # Checks if the chosen time for the new voyage overlaps with any existing voyage
-        # If so, the corresponding plane ID is removed from the list of all plane ID's leaving available Airplanes
+        # Get all airplanes that are not on a voyage at time of voyage
         for voyage in voyages_list:
             departure_out_str = voyage.get_departure_out()
             departure_out_temp = datetime.datetime.fromisoformat(departure_out_str)
